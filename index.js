a764923a0ffb177827920aa67a0979b5052dd575
Adam Balsam <adam.balsam@acquia.com>
2017-08-01 14:29:51 -0400

    Consume public data

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var baseurl = 'http://headlessnightlyth3vcdx4ci.devcloud.acquia-sites.com';
+var getblocks = '/jsonapi/block/block';
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,4 +18,25 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+// Get block information from Nightly Build (available to anon users)
+request({
+    url: baseurl + getblocks,
+    json: true
+}, function (error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+    var blocks_all = body;
+  }
+
+  // Convert response into array.
+  var blocks = [];
+  blocks_all.data.forEach(function(item) {
+    blocks.push(item);
+  });
+
+  // Make available to templates as a global var.
+  app.locals.blockdata = blocks;
+
 });
